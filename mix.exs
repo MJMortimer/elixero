e5574ab160f9b4c1870a78b9219df3e61a720a27
Matthew Mortimer <matthew.mortimer1@gmail.com>
2016-11-06 15:40:59 +1300

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule EliXero.Mixfile do
 
   def project do
     [app: :elixero,
-     version: "0.0.3",
+     version: "0.0.4",
      elixir: "~> 1.3",
      description: description(),
      package: package(),
