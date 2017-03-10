3dabb3f2d6ef11ecd9ab4a2194249e0575c9e6a9
Dev-matthewm <matthew.mortimer1@gmail.com>
2017-03-10 15:51:20 +1300

    bank_transactions.ex

---
+++ lib/elixero/core_api/bank_transactions.ex
@@ -0,0 +1,24 @@
+defmodule EliXero.CoreApi.BankTransactions do
+	@api_type :core	
+	@resource "banktransactions"
+
+	def find(client) do
+		EliXero.CoreApi.Common.find(client, @resource)
+	end
+
+	def find(client, identifier) do
+		EliXero.CoreApi.Common.find(client, @resource, identifier)
+	end
+
+	def filter(client, filter) do
+		EliXero.CoreApi.Common.filter(client, @resource, filter)
+	end
+
+	def create(client, bank_transactions_map) do
+		EliXero.CoreApi.Common.create(client, @resource, bank_transactions_map)
+	end
+
+	def update(client, identifier, bank_transactions_map) do
+		EliXero.CoreApi.Common.update(client, @resource, identifier, bank_transactions_map)
+	end
+end
