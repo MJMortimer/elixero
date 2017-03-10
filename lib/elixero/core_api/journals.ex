8988df58cded9f1be3c2b3ddc82305b3e562082b
Dev-matthewm <matthew.mortimer1@gmail.com>
2017-03-10 19:41:46 +1300

    journals.ex

---
+++ lib/elixero/core_api/journals.ex
@@ -0,0 +1,16 @@
+defmodule EliXero.CoreApi.Journals do
+	@api_type :core	
+	@resource "journals"
+
+	def find(client) do
+		EliXero.CoreApi.Common.find(client, @resource)
+	end
+
+	def find(client, identifier) do
+		EliXero.CoreApi.Common.find(client, @resource, identifier)
+	end
+
+	def filter(client, filter) do
+		EliXero.CoreApi.Common.filter(client, @resource, filter)
+	end
+end
