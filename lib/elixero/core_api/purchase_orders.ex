0c783c2bddc6b63290706d85c88334f07ecd1d73
Dev-matthewm <matthew.mortimer1@gmail.com>
2017-03-10 20:00:54 +1300

    purchase_orders.ex

---
+++ lib/elixero/core_api/purchase_orders.ex
@@ -0,0 +1,24 @@
+defmodule EliXero.CoreApi.PurchaseOrders do
+	@api_type :core	
+	@resource "purchaseorders"
+
+	def find(client) do
+		EliXero.CoreApi.Common.find(client, @resource)
+	end
+
+	def find(client, identifier) do
+		EliXero.CoreApi.Common.find(client, @resource, identifier)
+	end
+
+	def filter(client, filter) do
+		EliXero.CoreApi.Common.filter(client, @resource, filter)
+	end
+
+	def create(client, purchase_orders_map) do
+		EliXero.CoreApi.Common.create(client, @resource, purchase_orders_map)
+	end
+
+	def update(client, identifier, purchase_orders_map) do
+		EliXero.CoreApi.Common.update(client, @resource, identifier, purchase_orders_map)
+	end
+end
