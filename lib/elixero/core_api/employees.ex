3b860e486e2d0df781e46a7352ca0ce2322f0162
Dev-matthewm <matthew.mortimer1@gmail.com>
2017-03-10 17:41:38 +1300

    employees.ex

---
+++ lib/elixero/core_api/employees.ex
@@ -0,0 +1,24 @@
+defmodule EliXero.CoreApi.Employees do
+	@api_type :core	
+	@resource "employees"
+
+	def find(client) do
+		EliXero.CoreApi.Common.find(client, @resource)
+	end
+
+	def find(client, identifier) do
+		EliXero.CoreApi.Common.find(client, @resource, identifier)
+	end
+
+	def filter(client, filter) do
+		EliXero.CoreApi.Common.filter(client, @resource, filter)
+	end
+
+	def create(client, employees_map) do
+		EliXero.CoreApi.Common.create(client, @resource, employees_map)
+	end
+
+	def update(client, identifier, employees_map) do
+		EliXero.CoreApi.Common.update(client, @resource, identifier, employees_map)
+	end
+end
