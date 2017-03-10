d58bb6f2eb6c8028ba2b8a88ef22dd8fb01f0e62
Dev-matthewm <matthew.mortimer1@gmail.com>
2017-03-10 17:39:16 +1300

    currencies.ex

---
+++ lib/elixero/core_api/currencies.ex
@@ -0,0 +1,12 @@
+defmodule EliXero.CoreApi.Currencies do
+	@api_type :core	
+	@resource "currencies"
+
+	def find(client) do
+		EliXero.CoreApi.Common.find(client, @resource)
+	end
+
+	def filter(client, filter) do
+		EliXero.CoreApi.Common.filter(client, @resource, filter)
+	end
+end
