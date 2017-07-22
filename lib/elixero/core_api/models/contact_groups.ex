9f955d08614056690b9290a53189f7470a057f85
MJMortimer <matthew.mortimer1@gmail.com>
2017-07-23 01:41:49 +1200

    ContactGroups endpoint returns module formed data

--- lib/elixero/core_api/models/contact_groups.ex
+++ lib/elixero/core_api/models/contact_groups.ex
@@ -1,8 +1,16 @@
 defmodule EliXero.CoreApi.Models.ContactGroups do
     use Ecto.Schema
+    import Ecto.Changeset
     @derive {Poison.Encoder, except: [:__meta__, :id]}
 
     schema "contactgroups" do
         embeds_many :ContactGroups, EliXero.CoreApi.Models.ContactGroups.ContactGroup
+    end
+
+    def from_map(data) do
+        %__MODULE__{}
+        |> cast(data, [])
+        |> cast_embed(:ContactGroups)
+        |> apply_changes
     end
 end
