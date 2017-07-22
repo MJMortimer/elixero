7f6a7eab1b5163abcee84a7037d9b034cb8dc5bd
MJMortimer <matthew.mortimer1@gmail.com>
2017-07-23 02:12:50 +1200

    Currencies endpoint returns module formed data

--- lib/elixero/core_api/models/currencies.ex
+++ lib/elixero/core_api/models/currencies.ex
@@ -1,8 +1,17 @@
 defmodule EliXero.CoreApi.Models.Currencies do
     use Ecto.Schema
+    import Ecto.Changeset
+
     @derive {Poison.Encoder, except: [:__meta__, :id]}
 
     schema "currencies" do
         embeds_many :Currencies, EliXero.CoreApi.Models.Currencies.Currency
+    end
+
+    def from_map(data) do
+        %__MODULE__{}
+        |> cast(data, [])
+        |> cast_embed(:Currencies)
+        |> apply_changes
     end
 end
--- lib/elixero/core_api/models/currencies/currency.ex
+++ lib/elixero/core_api/models/currencies/currency.ex
@@ -1,5 +1,7 @@
 defmodule EliXero.CoreApi.Models.Currencies.Currency do
     use Ecto.Schema
+    import Ecto.Changeset
+
     @derive {Poison.Encoder, except: [:__meta__, :id]}
 
     schema "currencies" do
@@ -8,5 +10,12 @@ defmodule EliXero.CoreApi.Models.Currencies.Currency do
         embeds_many :ValidationErrors, EliXero.CoreApi.Models.Common.Error
         embeds_many :Warnings, EliXero.CoreApi.Models.Common.Warning
         field :StatusAttributeString, :string   
+    end
+
+    def changeset(struct, data) do
+        struct
+        |> cast(data, [:Code, :Description, :StatusAttributeString])
+        |> cast_embed(:ValidationErrors)
+        |> cast_embed(:Warnings)
     end
 end
