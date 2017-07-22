58b90ae31119393d44c900e9bb70ac3c3abc5c9c
MJMortimer <matthew.mortimer1@gmail.com>
2017-07-22 17:32:35 +1200

    Bank transfers  schema

---
+++ lib/elixero/core_api/models/bank_transfers/bank_transfer.ex
@@ -0,0 +1,20 @@
+defmodule EliXero.CoreApi.Models.BankTransfers.BankTransfer do
+    use Ecto.Schema
+    @derive {Poison.Encoder, except: [:__meta__]}
+
+    schema "banktransfers" do
+        field :BankTransferID, Ecto.UUID
+        embeds_one :FromBankAccount, EliXero.CoreApi.Models.Accounts.Account
+        embeds_one :ToBankAccount, EliXero.CoreApi.Models.Accounts.Accountfield :BankTransferID, Ecto.UUIDfield :BankTransferID, Ecto.UUID
+        field :FromBankTransactionID, Ecto.UUID
+        field :ToBankTransactionID, Ecto.UUID
+        field :CurrencyRate, :decimal
+        field :Amount, :decimal
+        field :Date, :string
+        field :HasAttachments, :boolean
+        field :UpdatedDateUTC, :string
+        embeds_many :ValidationErrors, EliXero.CoreApi.Models.Common.Error
+        embeds_many :Warnings, EliXero.CoreApi.Models.Common.Warning
+        field :StatusAttributeString, :string  
+    end
+end
---
+++ lib/elixero/core_api/models/bank_trnsfers.ex
@@ -0,0 +1,8 @@
+defmodule EliXero.CoreApi.Models.BankTransfers do
+    use Ecto.Schema
+    @derive {Poison.Encoder, except: [:__meta__]}
+
+    schema "banktransfers" do
+        embeds_many :BankTransfers, EliXero.CoreApi.Models.BankTransfers.BankTransfer
+    end
+end
