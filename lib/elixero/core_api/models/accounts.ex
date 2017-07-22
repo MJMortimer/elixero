3479ff52c3b943afa3bec146bbad7195bde759c8
MJMortimer <matthew.mortimer1@gmail.com>
2017-07-23 00:29:11 +1200

    Accounts endpoint returns module formed data

--- lib/elixero/core_api/models/accounts.ex
+++ lib/elixero/core_api/models/accounts.ex
@@ -1,8 +1,17 @@
 defmodule EliXero.CoreApi.Models.Accounts do
     use Ecto.Schema
+    import Ecto.Changeset
+
     @derive {Poison.Encoder, except: [:__meta__, :id]}
 
     schema "accounts" do
         embeds_many :Accounts, EliXero.CoreApi.Models.Accounts.Account
+    end
+
+    def from_map(data) do
+        %__MODULE__{}
+        |> cast(data, [])
+        |> cast_embed(:Accounts)
+        |> apply_changes
     end
 end
--- lib/elixero/core_api/models/accounts/account.ex
+++ lib/elixero/core_api/models/accounts/account.ex
@@ -1,6 +1,30 @@
 defmodule EliXero.CoreApi.Models.Accounts.Account do
     use Ecto.Schema
+    import Ecto.Changeset
+
     @derive {Poison.Encoder, except: [:__meta__, :id]}
+
+    @fields [    
+        :AccountID,
+        :Code,
+        :Name,
+        :Status,
+        :Type,
+        :BankAccountType,
+        :TaxType,
+        :Description,
+        :Class,
+        :SystemAccount,
+        :EnablePaymentsToAccount,
+        :ShowInExpenseClaims,
+        :BankAccountNumber,
+        :CurrencyCode,
+        :ReportingCode,
+        :ReportingCodeName,
+        :HasAttachments,
+        :UpdatedDateUTC,
+        :StatusAttributeString
+    ]
 
     schema "accounts" do
         field :AccountID, Ecto.UUID
@@ -24,5 +48,12 @@ defmodule EliXero.CoreApi.Models.Accounts.Account do
         embeds_many :ValidationErrors, EliXero.CoreApi.Models.Common.Error
         embeds_many :Warnings, EliXero.CoreApi.Models.Common.Warning
         field :StatusAttributeString, :string        
+    end
+
+    def changeset(struct, data) do
+        struct
+        |> cast(data, @fields)
+        |> cast_embed(:ValidationErrors)
+        |> cast_embed(:Warnings)
     end
 end
