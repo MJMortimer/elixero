856a65932858ddc787cfb08693bc7d035aed968e
MJMortimer <matthew.mortimer1@gmail.com>
2017-07-22 21:02:00 +1200

    Updated tax component schema

--- lib/elixero/core_api/models/tax_rates/tax_rate/tax_component.ex
+++ lib/elixero/core_api/models/tax_rates/tax_rate/tax_component.ex
@@ -5,6 +5,9 @@ defmodule EliXero.CoreApi.Models.TaxRates.TaxRate.TaxComponent do
     schema "taxcomponents" do
         field :Name, :string
         field :Rate, :string
-        field :IsCompound, :boolean        
+        field :IsCompound, :boolean
+        embeds_many :ValidationErrors, EliXero.CoreApi.Models.Common.Error
+        embeds_many :Warnings, EliXero.CoreApi.Models.Common.Warning
+        field :StatusAttributeString, :string
     end
 end
