60208834f6c192391c57b81bbf5290b922001345
MJMortimer <matthew.mortimer1@gmail.com>
2017-07-23 01:28:41 +1200

    BrandingThemes endpoint returns module formed data

--- lib/elixero/core_api/models/branding_themes.ex
+++ lib/elixero/core_api/models/branding_themes.ex
@@ -1,8 +1,16 @@
 defmodule EliXero.CoreApi.Models.BrandingThemes do
     use Ecto.Schema
+    import Ecto.Changeset
     @derive {Poison.Encoder, except: [:__meta__, :id]}
 
     schema "brandingthemes" do
         embeds_many :BrandingThemes, EliXero.CoreApi.Models.BrandingThemes.BrandingTheme
+    end
+
+    def from_map(data) do
+        %__MODULE__{}
+        |> cast(data, [])
+        |> cast_embed(:BrandingThemes)
+        |> apply_changes
     end
 end
