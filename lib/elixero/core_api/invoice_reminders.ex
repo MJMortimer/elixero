ac89be8a3e2aa421e137c9282078aa82c7a32457
MJMortimer <matthew.mortimer1@gmail.com>
2017-07-23 13:21:44 +1200

    InvocieReminders endpoint returns module formed data

--- lib/elixero/core_api/invoice_reminders.ex
+++ lib/elixero/core_api/invoice_reminders.ex
@@ -1,9 +1,11 @@
 defmodule EliXero.CoreApi.InvoiceReminders do
   @resource "invoicereminders"
+  @model_module EliXero.CoreApi.Models.InvoiceReminders
 
   def settings(client) do
     resource = @resource <> "/settings"
 
     EliXero.CoreApi.Common.find(client, resource)
+    |> EliXero.CoreApi.Utils.ResponseHandler.handle_response(@model_module)
   end
 end
--- lib/elixero/core_api/models/invoice_reminders.ex
+++ lib/elixero/core_api/models/invoice_reminders.ex
@@ -1,8 +1,17 @@
 defmodule EliXero.CoreApi.Models.InvoiceReminders do
     use Ecto.Schema
+    import Ecto.Changeset
+
     @derive {Poison.Encoder, except: [:__meta__, :id]}
 
     schema "invoiceReminders" do
         embeds_many :InvoiceReminders, EliXero.CoreApi.Models.InvoiceReminders.InvoiceReminder
+    end
+
+    def from_map(data) do
+        %__MODULE__{}
+        |> cast(data, [])
+        |> cast_embed(:InvoiceReminders)
+        |> apply_changes
     end
 end
