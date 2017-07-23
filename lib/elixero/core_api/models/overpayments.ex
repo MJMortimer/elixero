e126c082142296ce6b497b635bcebe41a8c393b9
MJMortimer <matthew.mortimer1@gmail.com>
2017-07-23 14:07:20 +1200

    Overpayments endpoint returns module formed data

--- lib/elixero/core_api/models/overpayments.ex
+++ lib/elixero/core_api/models/overpayments.ex
@@ -1,8 +1,17 @@
 defmodule EliXero.CoreApi.Models.Overpayments do
     use Ecto.Schema
+    import Ecto.Changeset
+    
     @derive {Poison.Encoder, except: [:__meta__, :id]}
 
     schema "overpayments" do
         embeds_many :Overpayments, EliXero.CoreApi.Models.Overpayments.Overpayment
+    end
+
+    def from_map(data) do
+        %__MODULE__{}
+        |> cast(data, [])
+        |> cast_embed(:Overpayments)
+        |> apply_changes
     end
 end
