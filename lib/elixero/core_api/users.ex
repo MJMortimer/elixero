00d9ca5691acde51fa12b6f10c27d35c7a850619
MJMortimer <matthew.mortimer1@gmail.com>
2017-07-23 14:45:25 +1200

    Users endpoint returns module formed data

--- lib/elixero/core_api/users.ex
+++ lib/elixero/core_api/users.ex
@@ -1,15 +1,19 @@
 defmodule EliXero.CoreApi.Users do
   @resource "users"
+  @model_module EliXero.CoreApi.Models.Users
 
   def find(client) do
     EliXero.CoreApi.Common.find(client, @resource)
+    |> EliXero.CoreApi.Utils.ResponseHandler.handle_response(@model_module)
   end
 
   def find(client, identifier) do
     EliXero.CoreApi.Common.find(client, @resource, identifier)
+    |> EliXero.CoreApi.Utils.ResponseHandler.handle_response(@model_module)
   end
 
   def filter(client, filter) do
     EliXero.CoreApi.Common.filter(client, @resource, filter)
+    |> EliXero.CoreApi.Utils.ResponseHandler.handle_response(@model_module)
   end
 end
