4b625b28adb174f801943afd9f897f7b93dab529
MJMortimer <matthew.mortimer1@gmail.com>
2017-07-23 13:38:40 +1200

    Invocies endpoint returns module formed data

--- lib/elixero/core_api/models/online_invoices.ex
+++ lib/elixero/core_api/models/online_invoices.ex
@@ -1,17 +1,17 @@
-defmodule EliXero.CoreApi.Models.Invoices do
+defmodule EliXero.CoreApi.Models.OnlineInvoices do
     use Ecto.Schema
     import Ecto.Changeset
-
+    
     @derive {Poison.Encoder, except: [:__meta__, :id]}
 
-    schema "invoices" do
-        embeds_many :Invoices, EliXero.CoreApi.Models.Invoices.Invoice
+    schema "onlineinvoices" do
+        embeds_many :OnlineInvoices, EliXero.CoreApi.Models.OnlineInvoices.OnlineInvoice
     end
 
     def from_map(data) do
         %__MODULE__{}
         |> cast(data, [])
-        |> cast_embed(:Invoices)
+        |> cast_embed(:OnlineInvoices)
         |> apply_changes
     end
 end
