f1aa5e0f26eb680220226aa181162ea69a6e258c
MJMortimer <matthew.mortimer1@gmail.com>
2017-07-23 14:11:34 +1200

    Prepayments endpoint returns module formed data

--- lib/elixero/core_api/models/prepayments.ex
+++ lib/elixero/core_api/models/prepayments.ex
@@ -1,8 +1,17 @@
 defmodule EliXero.CoreApi.Models.Prepayments do
     use Ecto.Schema
+    import Ecto.Changeset
+    
     @derive {Poison.Encoder, except: [:__meta__, :id]}
 
     schema "prepayments" do
         embeds_many :Prepayments, EliXero.CoreApi.Models.Prepayments.Prepayment
+    end
+
+    def from_map(data) do
+        %__MODULE__{}
+        |> cast(data, [])
+        |> cast_embed(:Prepayments)
+        |> apply_changes
     end
 end
