e4ef5797c4fe23a25b8514f1055cccf1f8c4b5c9
MJMortimer <matthew.mortimer1@gmail.com>
2017-07-23 14:18:51 +1200

    Receipts endpoint returns module formed data

--- lib/elixero/core_api/receipts.ex
+++ lib/elixero/core_api/receipts.ex
@@ -1,23 +1,29 @@
 defmodule EliXero.CoreApi.Receipts do
   @resource "receipts"
+  @model_module EliXero.CoreApi.Models.Receipts
 
   def find(client) do
     EliXero.CoreApi.Common.find(client, @resource)
+    |> EliXero.CoreApi.Utils.ResponseHandler.handle_response(@model_module)
   end
 
   def find(client, identifier) do
     EliXero.CoreApi.Common.find(client, @resource, identifier)
+    |> EliXero.CoreApi.Utils.ResponseHandler.handle_response(@model_module)
   end
 
   def filter(client, filter) do
     EliXero.CoreApi.Common.filter(client, @resource, filter)
+    |> EliXero.CoreApi.Utils.ResponseHandler.handle_response(@model_module)
   end
 
   def create(client, receipts_map) do
     EliXero.CoreApi.Common.create(client, @resource, receipts_map)
+    |> EliXero.CoreApi.Utils.ResponseHandler.handle_response(@model_module)
   end
 
   def update(client, identifier, receipts_map) do
     EliXero.CoreApi.Common.update(client, @resource, identifier, receipts_map)
+    |> EliXero.CoreApi.Utils.ResponseHandler.handle_response(@model_module)
   end
 end
