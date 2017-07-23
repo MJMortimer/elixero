6ecea1f7781fd18af9929d5a479e590c5bfe2962
MJMortimer <matthew.mortimer1@gmail.com>
2017-07-23 14:04:25 +1200

    Organisations endpoint returns module formed data

--- lib/elixero/core_api/models/organisations.ex
+++ lib/elixero/core_api/models/organisations.ex
@@ -1,8 +1,17 @@
 defmodule EliXero.CoreApi.Models.Organisations do
     use Ecto.Schema
+    import Ecto.Changeset
+    
     @derive {Poison.Encoder, except: [:__meta__, :id]}
 
     schema "organisations" do
         embeds_many :Organisations, EliXero.CoreApi.Models.Organisations.Organisation
+    end
+
+    def from_map(data) do
+        %__MODULE__{}
+        |> cast(data, [])
+        |> cast_embed(:Organisations)
+        |> apply_changes
     end
 end
--- lib/elixero/core_api/models/organisations/organisation.ex
+++ lib/elixero/core_api/models/organisations/organisation.ex
@@ -1,6 +1,34 @@
 defmodule EliXero.CoreApi.Models.Organisations.Organisation do
     use Ecto.Schema
+    import Ecto.Changeset
+
     @derive {Poison.Encoder, except: [:__meta__, :id]}
+
+    @fields [
+        :OrganisationID,
+        :Name,
+        :LegalName,
+        :ShortCode,
+        :PaysTax,
+        :Version,
+        :OrganisationEntityType,
+        :IsDemoCompany,
+        :BaseCurrency,
+        :ApiKey,
+        :CountryCode,
+        :OrganisationStatus,
+        :TaxNumber,
+        :FinancialYearEndDay,
+        :FinancialYearEndMonth,
+        :PeriodLockDate,
+        :EndOfYearLockDate,
+        :CreatedDateUTC,
+        :Timezone,
+        :LineOfBusiness,
+        :RegistrationNumber,
+        :SalesTaxBasisType,
+        :SalesTaxPeriod
+    ]
 
     schema "organisations" do
         field :OrganisationID, Ecto.UUID
@@ -30,5 +58,14 @@ defmodule EliXero.CoreApi.Models.Organisations.Organisation do
         embeds_many :Addresses, EliXero.CoreApi.Models.Common.Address
         embeds_many :Phones, EliXero.CoreApi.Models.Common.Phone
         embeds_many :ExternalLinks, EliXero.CoreApi.Models.Common.ExternalLink
+    end
+
+    def changeset(struct, data) do
+        struct
+        |> cast(data, @fields)
+        |> cast_embed(:PaymentTerms)
+        |> cast_embed(:Addresses)
+        |> cast_embed(:Phones)
+        |> cast_embed(:ExternalLinks)
     end
 end
