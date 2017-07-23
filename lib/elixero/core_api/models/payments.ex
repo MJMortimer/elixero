d0ece44e0662ae2175259d162455044689a3dccb
MJMortimer <matthew.mortimer1@gmail.com>
2017-07-23 14:09:23 +1200

    Payments endpoint returns module formed data

--- lib/elixero/core_api/models/payments.ex
+++ lib/elixero/core_api/models/payments.ex
@@ -1,8 +1,17 @@
 defmodule EliXero.CoreApi.Models.Payments do
     use Ecto.Schema
+    import Ecto.Changeset
+
     @derive {Poison.Encoder, except: [:__meta__, :id]}
 
     schema "payments" do
         embeds_many :Payments, EliXero.CoreApi.Models.Payments.Payment
+    end
+
+    def from_map(data) do
+        %__MODULE__{}
+        |> cast(data, [])
+        |> cast_embed(:Payments)
+        |> apply_changes
     end
 end
--- lib/elixero/core_api/payments.ex
+++ lib/elixero/core_api/payments.ex
@@ -1,23 +1,29 @@
 defmodule EliXero.CoreApi.Payments do
   @resource "payments"
+  @model_module EliXero.CoreApi.Models.Payments
 
   def find(client) do
     EliXero.CoreApi.Common.find(client, @resource)
+    |> EliXero.CoreApi.Utils.ResponseHandler.handle_response(@model_module)
   end
 
   def find(client, identifier) do
     EliXero.CoreApi.Common.find(client, @resource, identifier)
+    |> EliXero.CoreApi.Utils.ResponseHandler.handle_response(@model_module)
   end
 
   def filter(client, filter) do
     EliXero.CoreApi.Common.filter(client, @resource, filter)
+    |> EliXero.CoreApi.Utils.ResponseHandler.handle_response(@model_module)
   end
 
   def create(client, payments_map) do
     EliXero.CoreApi.Common.create(client, @resource, payments_map)
+    |> EliXero.CoreApi.Utils.ResponseHandler.handle_response(@model_module)
   end
 
   def update(client, identifier, payments_map) do
     EliXero.CoreApi.Common.update(client, @resource, identifier, payments_map)
+    |> EliXero.CoreApi.Utils.ResponseHandler.handle_response(@model_module)
   end
 end
