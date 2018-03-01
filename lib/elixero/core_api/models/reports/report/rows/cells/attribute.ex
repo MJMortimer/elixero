c853a2ea9787f5670103e047fd55e9f9e306bb69
Eric Sullivan <eric.sullivan@annkissam.com>
2018-03-01 15:17:01 -0500

    fix report changeset

--- lib/elixero/core_api/models/reports/report/rows/cells/attribute.ex
+++ lib/elixero/core_api/models/reports/report/rows/cells/attribute.ex
@@ -1,16 +1,16 @@
 defmodule EliXero.CoreApi.Models.Reports.Report.Rows.Cell.Attribute do
-    use Ecto.Schema
-    import Ecto.Changeset
-    
-    @derive {Poison.Encoder, except: [:__meta__, :id]}
+  use Ecto.Schema
+  import Ecto.Changeset
 
-    schema "attributes" do
-        field :Value, :string
-        field :Id, :string
-    end
+  @derive {Poison.Encoder, except: [:__meta__, :id]}
 
-    def changeset(struct, data) do
-        struct
-        |> cast(data, [:field, :Id])
-    end
+  schema "attributes" do
+    field :Value, :string
+    field :Id, :string
+  end
+
+  def changeset(struct, data) do
+    struct
+    |> cast(data, [:Value, :Id])
+  end
 end
